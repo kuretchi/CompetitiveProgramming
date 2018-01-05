fa1e4e450db1598977f468e3d3f1245eae4d19cf
kuretchi <kuretchi@outlook.jp>
2018-01-06 04:40:44 +0900

    Add Eratosthenes.cs

---
+++ CompetitiveProgramming/Math/Eratosthenes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace CompetitiveProgramming.Math
+{
+    public static class Eratosthenes
+    {
+        public static IEnumerable<int> Sieve(int maxValue)
+        {
+            if (maxValue < 2) yield break;
+            yield return 2;
+
+            var flgs = new bool[maxValue + 1];
+            var i = 3;
+            for (i = 3; i * i < maxValue; i += 2)
+                if (!flgs[i])
+                {
+                    yield return i;
+                    for (var j = i * (i | 1); j <= maxValue; j += i << 1)
+                        flgs[j] = true;
+                }
+
+            for (; i <= maxValue; i += 2)
+                if (!flgs[i])
+                    yield return i;
+        }
+    }
+}
