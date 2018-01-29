5165a901332cb1468113f63a82c246c0602816b1
kuretchi <kuretchi@outlook.jp>
2018-01-29 17:40:47 +0900

    Add Constants.cs

---
+++ CompetitiveProgramming/Extensions/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.Extensions
+{
+    public static class Constants
+    {
+        public const int AnswerToLifeTheUniverseAndEverything = 42;
+        public const string Yes = nameof(Yes);
+        public const string No = nameof(No);
+        public const string YES = nameof(YES);
+        public const string NO = nameof(NO);
+        public const string Possible = nameof(Possible);
+        public const string Impossible = nameof(Impossible);
+    }
+}
