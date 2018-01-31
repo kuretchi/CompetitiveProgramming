a9a6610f2224bfc638e9843369cd2cb1d79a4540
kuretchi <kuretchi@outlook.jp>
2018-02-01 04:14:40 +0900

    Remove Zip

--- CompetitiveProgramming.CSharp7/Extensions/Extensions.cs
+++ CompetitiveProgramming.CSharp7/Extensions/Extensions.cs
@@ -37,28 +37,5 @@ namespace CompetitiveProgramming.CSharp7.Extensions
             foreach (var (t1, t2, t3, t4) in source) { ts1[i] = t1; ts2[i] = t2; ts3[i] = t3; ts4[i] = t4; i++; }
             return (ts1, ts2, ts3, ts4);
         }
-
-        public static IEnumerable<T> Zip<T1, T2, T3, T>(this IEnumerable<T1> first, IEnumerable<T2> second, IEnumerable<T3> thrid, Func<T1, T2, T3, T> resultSelector)
-        {
-            using (var e1 = first.GetEnumerator())
-            using (var e2 = second.GetEnumerator())
-            using (var e3 = thrid.GetEnumerator())
-            {
-                while (e1.MoveNext() && e2.MoveNext() && e3.MoveNext())
-                    yield return resultSelector(e1.Current, e2.Current, e3.Current);
-            }
-        }
-
-        public static IEnumerable<T> Zip<T1, T2, T3, T4, T>(this IEnumerable<T1> first, IEnumerable<T2> second, IEnumerable<T3> thrid, IEnumerable<T4> fourth, Func<T1, T2, T3, T4, T> resultSelector)
-        {
-            using (var e1 = first.GetEnumerator())
-            using (var e2 = second.GetEnumerator())
-            using (var e3 = thrid.GetEnumerator())
-            using (var e4 = fourth.GetEnumerator())
-            {
-                while (e1.MoveNext() && e2.MoveNext() && e3.MoveNext() && e4.MoveNext())
-                    yield return resultSelector(e1.Current, e2.Current, e3.Current, e4.Current);
-            }
-        }
     }
 }
