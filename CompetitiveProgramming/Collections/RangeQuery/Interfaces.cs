0f223d644787ca7822513eb278a8f52cbf262c2d
kuretchi <kuretchi@outlook.jp>
2018-03-04 00:05:17 +0900

    [RangeQuery] Add interfaces of queries

---
+++ CompetitiveProgramming/Collections/RangeQuery/Interfaces.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.Collections.RangeQuery
+{
+    public interface IPointGettable<T>
+    {
+        T GetAt(int i);
+    }
+
+    public interface IPointSettable<T>
+    {
+        void SetAt(int i, T value);
+    }
+
+    public interface IPrefixRangeConcatable<T>
+    {
+        T Concat(int r);
+    }
+
+    public interface IRangeConcatable<T>
+    {
+        T Concat(int l, int r);
+    }
+
+    public interface IPointAppendable<T>
+    {
+        void AppendAt(int i, T value);
+    }
+
+    public interface IRangeAppendable<T>
+    {
+        void Append(int l, int r, T value);
+    }
+}
