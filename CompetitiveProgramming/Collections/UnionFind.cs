5791b7c5ce9e4f2ad2e7195592855c3ea4a9a253
kuretchi <kuretchi@outlook.jp>
2018-03-05 14:38:14 +0900

    [UnionFind] Refactor / Add ToLookup

--- CompetitiveProgramming/Collections/UnionFind.cs
+++ CompetitiveProgramming/Collections/UnionFind.cs
@@ -13,6 +13,7 @@ namespace CompetitiveProgramming.Collections
         public UnionFind(int length)
         {
             _tree = Enumerable.Repeat(-1, length).ToArray();
+            this.GroupCount = length;
         }
 
         public bool Unite(int i, int j)
@@ -23,15 +24,30 @@ namespace CompetitiveProgramming.Collections
             if (_tree[i] > _tree[j]) { var t = i; i = j; j = t; }
             _tree[i] += _tree[j];
             _tree[j] = i;
+            this.GroupCount--;
             return true;
         }
 
-        public bool AreUnited(int i, int j) => Find(i) == Find(j);
+        public int Length => _tree.Length;
 
-        public int Count(int i) => -_tree[Find(i)];
+        public int GroupCount { get; private set; }
 
-        public int CountGroups() => _tree.Count(x => x < 0);
+        public bool AreUnited(int i, int j)
+            => Find(i) == Find(j);
 
-        public int Find(int i) => _tree[i] < 0 ? i : _tree[i] = Find(_tree[i]);
+        public int Count(int i)
+            => -_tree[Find(i)];
+
+        public int Find(int i)
+            => _tree[i] < 0 ? i : _tree[i] = Find(_tree[i]);
+
+        public ILookup<int, int> ToLookup()
+            => Enumerate().ToLookup(kvp => kvp.Key, kvp => kvp.Value);
+
+        private IEnumerable<KeyValuePair<int, int>> Enumerate()
+        {
+            for (var i = 0; i < this.Length; i++)
+                yield return new KeyValuePair<int, int>(this.Find(i), i);
+        }
     }
 }
